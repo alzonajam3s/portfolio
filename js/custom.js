b43d593d63335b32aab9b03b117a91051f9f5a6a
alzonajam3s <35958800+alzonajam3s@users.noreply.github.com>
2018-07-24 20:05:46 +0800

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -8,14 +8,14 @@ $(document).ready(function(){
         scroll_start = $(this).scrollTop();
         if(scroll_start > offset.top) {
               $(".navbar-default").css('background-color', 'rgba(0, 0, 0, 0.5)');
-              $(".kulay").css('color', 'rgba(255, 255, 255, 0.8)');
-              $(".first-set").css('color', 'transparent');
-              $(".second-set").css('display', 'none');
+//              $(".kulay").css('color', 'rgba(255, 255, 255, 0.8)');
+//              $(".first-set").css('color', 'transparent');
+//              $(".second-set").css('display', 'none');
            } else {
               $('.navbar-default').css('background-color', 'transparent');
-              $('.kulay').css('color', 'rgba(0, 0, 0, 0.7)');
-              $(".first-set").css('color', 'rgba(0, 0, 0, 0.75)');
-              $(".second-set").css('display', '');
+//              $('.kulay').css('color', 'rgba(0, 0, 0, 0.7)');
+//             $(".first-set").css('color', 'rgba(0, 0, 0, 0.75)');
+//              $(".second-set").css('display', '');
            }
        });
     }
